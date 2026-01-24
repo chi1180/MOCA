2fbb6bd7a9efaaa9a6555416d2af5d796be1fbf9
chi1180 <143685104+chi1180@users.noreply.github.com>
2026-01-24 22:16:37 +0900

    Type polygonData as GeoJsonObject in MAP_DATA

--- data/data.ts
+++ data/data.ts
@@ -1,8 +1,9 @@
 import L from "leaflet";
 import polygonData from "./polygon.json";
+import type { GeoJsonObject } from "geojson";
 
 export const MAP_DATA = {
   center: [34.533557, 132.750555] as L.LatLngExpression,
-  polygon: polygonData,
+  polygon: polygonData as GeoJsonObject,
   scale: 13.2,
 };
